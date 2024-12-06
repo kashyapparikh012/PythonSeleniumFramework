d02c8bb0c11ed49a67e86fd37c665c83530e07bd
Kashyap <kashyapparikh012@gmail.com>
2024-12-06 15:14:20 -0500

    removing code block that is not needed

--- testData/excelDemo.py
+++ testData/excelDemo.py
@@ -2,16 +2,6 @@ import openpyxl
 Dict = {}   # Dict: <class 'dict'>: {'firstname': 'Meet', 'email': 'meet@gmail.com'}
 book = openpyxl.load_workbook("C:/Users/kashy/PycharmProjects/PythonSelFramework/PythonDemo.xlsx")
 sheet = book.active
-# cell = sheet.cell(row=1, column=2)
-# print(cell.value)
-#
-# sheet.cell(row=2, column=2).value = "Meet"
-# print(sheet.cell(row=2, column=2).value)
-#
-# print(sheet.max_row)
-# print(sheet.max_column)
-#
-# print(sheet['A3'].value)
 
 for i in range(1, sheet.max_row+1): # To get rows
     if sheet.cell(row=i, column=1).value == "TestCase1":
